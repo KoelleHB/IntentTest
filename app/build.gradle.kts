8f964ebdbc1a5f9361354083fda83147563155f5
KoelleHB <pwjulius@gmail.com>
2026-09-18 12:42:37 +0200

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application") }
+android { namespace = "de.patrick.shareintentdiagnostic"; compileSdk = 36
+    defaultConfig { applicationId = "de.patrick.shareintentdiagnostic"; minSdk = 23; targetSdk = 36; versionCode = 1; versionName = "1.0" }
+}
